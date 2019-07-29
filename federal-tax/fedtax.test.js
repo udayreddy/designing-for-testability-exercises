518bd8881f4f9303c0265b44ae4084f76ff6d1ad
ulakshminar1 <ulakshminar1@bloomberg.net>
2019-07-29 14:58:30 -0400

    default tax

--- federal-tax/fedtax.test.js
+++ federal-tax/fedtax.test.js
@@ -1,10 +1,16 @@
-import { calulateTax } from "./fedtax";
+import { calulateTax } from './fedtax';
 
-test("When you pay no taxes", () => {
-  expect(calulateTax(0, 0, 0)).toBe(0);
+test('When you have no income', () => {
+    expect(calulateTax(0, 0, 0)).toBe(0);
+    expect(calulateTax(70, 0, 0)).toBe(0);
+    expect(calulateTax(0, 90, 0)).toBe(0);
+    expect(calulateTax(70, 90, 0)).toBe(0);
 });
 
+test('Single , income higher than all brackets', () => {
+    expect(calulateTax(0, 0, 10000)).toBeCloseTo(3103.48);
+});
 
-test("single with 2 allowance increment", () => {
-    expect(calulateTax(0, 0, 0)).toBe(0);
-  });
+test('Married , income higher than all brackets', () => {
+    expect(calulateTax(1, 0, 10000)).toBeCloseTo(2854.45);
+});
